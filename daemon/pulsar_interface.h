17f1039bfc636fcc7d15c0818f0a10e658b9650f
nauqnew <dozenthree@gmail.com>
2017-02-08 13:50:30 +0800

    pulsar_interface.h changed

--- daemon/pulsar_interface.h
+++ daemon/pulsar_interface.h
@@ -1,1 +1,6 @@
+#ifndef PULSAR_INTERFACE_H
+#define PULSAR_INTERFACE_H
+
 int send_to_pulsar(const char *cdata, const char *pulsar_host, const char *pulsar_port, const char *pulsar_topic);
+
+#endif //PULSAR_INTERFACE_H
